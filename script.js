cafe8d6bf91f9ad1dd51ec85af62d1ebd39704f0
Jawad Adil <adiljay05@gmail.com>
2021-04-03 23:35:16 +0000

    Step 1: Fetch pre-filled link to google form.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Step 1:
+
+/*  Create a form and get pre-filled URL    *
+*   This is my link for pre-filled form     */
+//  https://docs.google.com/forms/d/e/1FAIpQLSfNC4OzmzvRC7JmODkrYeQWEixpy3_4iPvbmuPgMvBYSLkCgg/viewform?usp=pp_url&entry.776520014=24&entry.681507226=5.8&entry.516070196=Male&entry.1747364981=Yes
+
+
