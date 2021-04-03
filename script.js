8be09e12fb80fdd309b0e8f4e5f86b62a9824150
Jawad Adil <adiljay05@gmail.com>
2021-04-03 23:39:34 +0000

    Step 2: Modification in URL

--- script.js
+++ script.js
@@ -4,4 +4,15 @@
 *   This is my link for pre-filled form     */
 //  https://docs.google.com/forms/d/e/1FAIpQLSfNC4OzmzvRC7JmODkrYeQWEixpy3_4iPvbmuPgMvBYSLkCgg/viewform?usp=pp_url&entry.776520014=24&entry.681507226=5.8&entry.516070196=Male&entry.1747364981=Yes
 
+// Step 2:
+/*  Change your Form Link                   *
+*   Make Following changes in your URL      *
+*   1: Change 'viewform' to 'formResponse'  *
+*   2: Add this at the end of URL:          *
+*       '&submit=SUBMIT' (without quotes)   *
+*                                           *
+*   Your new link will look like this       */
+//  https://docs.google.com/forms/d/e/1FAIpQLSfNC4OzmzvRC7JmODkrYeQWEixpy3_4iPvbmuPgMvBYSLkCgg/formResponse?usp=pp_url&entry.776520014=24&entry.681507226=5.8&entry.516070196=Male&entry.1747364981=Yes&submit=SUBMIT
+
+
 
